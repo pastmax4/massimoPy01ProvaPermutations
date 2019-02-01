4b7c3364d58781c49dfdf4c33a891dd653f51da8
pastmax4 <46377444+pastmax4@users.noreply.github.com>
2019-02-01 10:36:06 +0100

    Add files via upload

--- pyPermutProva01.py
+++ pyPermutProva01.py
@@ -7,60 +7,32 @@ https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python/
 
 '''
 
+Keys=[]
+N=4
+jTot=1
+TreeInd = 0
 
+PermutationCode = {}
+for i in range(N,-1,-1):
 
-def permutation(lst): 
-  
-    # If lst is empty then there are no permutations 
-    if len(lst) == 0: 
-        return [] 
-  
-    # If there is only one element in lst then, only 
-    # one permuatation is possible 
-    if len(lst) == 1: 
-        return [lst] 
-  
-    # Find the permutations for lst if there are 
-    # more than 1 characters 
-  
-    l = [] # empty list that will store current permutation 
-  
-    # Iterate the input(lst) and calculate the permutation 
-    print('len di lst=',len(lst))
-    for i in range(len(lst)):   
-        print('##i=',i)           
-        m = lst[i] 
-        print('##m=  ',m)
-        
-        
-        # Extract lst[i] or m from the list.  remLst is 
-        # remaining list 
-        remLst = lst[:i] + lst[i+1:] 
-        print('remLlst  ', remLst)
-        print('  ')
-       
-        # Generating all permutations where m is first 
-        # element 
-        for p in permutation(remLst): 
-            print('ppppppp=',p)
-            l.append([m] + p)
-            
-        print('---------------------')     
-    return l 
-
-
-listPippo=['a','b','c','d','e']
-
-data = list('123') 
-for p in permutation(data): 
-    print(p) 
-
-
-
-
-
-
-
-
+    print('---',i)
+    jTot=jTot*i
     
+    Fj=-1
+    for j in range(0,jTot,1):      
+        
+        if(j%i == 0):
+            Fj=Fj+1
+            print('')
+            
+        Keys=[N-i+1,j]     
+        FatherKeys = [N-i,Fj]
+             
+        #print('Father:', N-i,Fj ,'Key:',N-i+1,j)
+        
+        print('Father:', FatherKeys ,'Key:', Keys)
+        PermutationCode[str(FatherKeys[0])+ '|' + str(FatherKeys[1])+' '+str(Keys[0]) + '|' + str(Keys[1])]='Ciao'     
 
+
+for el in PermutationCode:
+    print(el,PermutationCode[el])
